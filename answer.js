bc0f98962feaf6306980b8ca9ffa8684314e711b
Mahmud Al Jubaer{usama} <85607199+codxcript@users.noreply.github.com>
2026-09-04 00:44:39 +0600

    Add getDayType function to classify daysq

--- answer.js
+++ answer.js
@@ -3,3 +3,27 @@
 function describeValue(value) {
   return `${typeof value} | ${value ? "truthy" : "falsy"}`;
 }
+
+// question 2:
+
+function getDayType(day){
+    switch(day.toLowerCase()){
+        case "friday":
+            return "Weekend";
+        case "saturday":
+            return "Weekend";
+        case "sunday":
+            return "Working Day";
+        case "monday":
+            return "Working Day";
+        case "tuesday":
+            return "Working Day";
+        case "wednesday":
+            return "Working Day";
+        case "thursday":
+            return "Working Day";
+        default:
+            return "Invalid Day";
+
+    }
+}
